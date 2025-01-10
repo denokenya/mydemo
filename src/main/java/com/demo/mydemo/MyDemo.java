329e8d4ec1cd75e0c408547b085c03eef06ddfde
denotech <dennismuteti25@gmail.com>
2025-01-10 13:10:18 +0300

    initial commit

---
+++ src/main/java/com/demo/mydemo/MyDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.demo.mydemo;
+
+/**
+ *
+ * @author dennismuteti
+ */
+public class MyDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
