43aead319a6bedfa242d157afa58331b551c33d3
denotech <dennismuteti25@gmail.com>
2025-01-11 11:57:03 +0300

    initial commit

---
+++ src/main/java/com/demo/mydemo/Example2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.demo.mydemo;
+
+/**
+ *
+ * @author dennismuteti
+ * This demonstrates a variable
+ */
+public class Example2 {
+    public static void main(String args[]){
+        int myVar1; // this declares a variable
+        int myVar2; //this declares another variable
+        
+        myVar1 = 1024; //This assigns 1024 to mayVar1
+        System.out.println("myVar1 contains " + myVar1);
+        
+        myVar2 = myVar1 /2;
+         System.out.print("myVar2 contains myVar1  / 2 :");
+         System.out.println(myVar2);
+         
+         
+    }
+    
+}
--- src/main/java/com/demo/mydemo/MyDemo.java
+++ src/main/java/com/demo/mydemo/MyDemo.java
@@ -9,6 +9,7 @@ package com.demo.mydemo;
  * @author dennismuteti
  */
 public class MyDemo {
+    // A java progran begins with a call to main()
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
