45f78bfdd64a1d2dbf0a099a79ebc665133d7dc3
denotech <dennismuteti25@gmail.com>
2025-01-11 12:03:04 +0300

    initial commit

--- src/main/java/com/demo/mydemo/MyDemo.java
+++ src/main/java/com/demo/mydemo/MyDemo.java
@@ -13,5 +13,15 @@ public class MyDemo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        int myVar1; // this declares a variable
+        int myVar2; //this declares another variable
+        
+        myVar1 = 1024; //This assigns 1024 to mayVar1
+        System.out.println("myVar1 contains " + myVar1);
+        
+        myVar2 = myVar1 /2;
+        System.out.print("myVar2 contains myVar1  / 2 :");
+        System.out.println(myVar2);
     }
 }
